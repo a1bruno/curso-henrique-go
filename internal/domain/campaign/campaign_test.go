f904413143478fd6834ddb0c880183a6f637540e
a1bruno <brunotrdev@gmail.com>
2026-06-25 18:09:59 -0300

    test: testa as demais propriedades essenciais da campanha

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -17,4 +18,25 @@ func Test_NewCampaign_Create_Campaign(t *testing.T) {
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_ID_Is_Not_Nil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Header"
+	contacts := []string{"e_mail1@me.com", "e_mail2@me.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOn_Greater_Than_Now(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Header"
+	contacts := []string{"e_mail1@me.com", "e_mail2@me.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+	assert.Greater(campaign.CreatedOn, now)
 }
